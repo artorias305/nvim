ceebdd9cd5c5f415c4bd41570c58d69abc4e6e73
artorias305 <caique.andraus@gmail.com>
2025-02-07 17:55:40 -0300

    initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "sainnhe/sonokai",
+    config = function()
+      vim.g.sonokai_transparent_background = "1"
+      vim.g.sonokai_enable_italic = "1"
+      vim.g.sonokai_style = "andromeda"
+    end,
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    opts = {},
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+  },
+  {
+    "sainnhe/gruvbox-material",
+  },
+  {
+    "aktersnurra/no-clown-fiesta.nvim",
+  },
+  {
+    "rebelot/kanagawa.nvim",
+  },
+  {
+    "olivercederborg/poimandres.nvim",
+  },
+  {
+    "datsfilipe/vesper.nvim",
+  },
+  {
+    "kepano/flexoki-neovim",
+  },
+}
