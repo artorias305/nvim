a41a780dd1ed146eabbf2080020d4fe1bdd763da
artorias305 <105161738+artorias305@users.noreply.github.com>
2025-07-26 11:12:08 -0300

    Add odin language support & gruber darker colorscheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -60,10 +60,13 @@ return {
 				opts = function(_, opts)
 					if (vim.g.colors_name or ""):find("catppuccin") then
 						opts.highlights = require("catppuccin.groups.integrations.bufferline")
-						.get()
+						    .get()
 					end
 				end
 			}
 		}
+	},
+	{
+		"blazkowolf/gruber-darker.nvim"
 	}
 }
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -24,6 +24,7 @@ return {
 			},
 			clangd = {},
 			ts_ls = {},
+			ols = {},
 			gopls = {
 				settings = {
 					gopls = {
