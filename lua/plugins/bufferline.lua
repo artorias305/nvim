68c6e1d771ada4b33a3245df86c09089cf04f24c
artorias305 <105161738+artorias305@users.noreply.github.com>
2025-07-27 17:54:30 -0300

    Add bufferline.nvim

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,15 @@
+return {
+	"akinsho/bufferline.nvim",
+	config = function()
+		require("bufferline").setup({
+			options = {
+				mode = "buffers",
+				numbers = "ordinal",
+				show_buffer_close_icons = false,
+				show_close_icon = false,
+				show_tab_indicators = false,
+				always_show_bufferline = false,
+			},
+		})
+	end
+}
