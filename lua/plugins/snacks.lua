ea3b75c13ba7bf957a048109f9e68f01dfb19a25
artorias305 <105161738+artorias305@users.noreply.github.com>
2025-08-31 16:33:32 -0300

    Remove these snacks some

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,11 +2,11 @@ return {
 	"folke/snacks.nvim",
 	lazy = false,
 	opts = {
-		dashboard = { enabled = true },
-		image = { enabled = true },
-		notifier = { enabled = true },
-		lazygit = { enabled = true },
-		quickfile = { enabled = true },
+		-- dashboard = { enabled = true },
+		-- image = { enabled = true },
+		-- notifier = { enabled = true },
+		-- lazygit = { enabled = true },
+		-- quickfile = { enabled = true },
 		terminal = { enabled = true }
 	},
 	keys = {
