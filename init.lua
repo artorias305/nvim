2c6e921d2f4f4112d97c066e38205315cce28ae7
artorias305 <105161738+artorias305@users.noreply.github.com>
2025-10-02 12:51:35 -0300

    Continue

--- init.lua
+++ init.lua
@@ -1,14 +1,15 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
 vim.opt.wrap = false
 vim.opt.tabstop = 4
 vim.opt.swapfile = false
 vim.g.mapleader = " "
+vim.opt.winborder = "rounded"
 
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
 vim.pack.add({
 	{ src = "https://github.com/vague2k/vague.nvim" },
@@ -18,7 +19,13 @@ vim.pack.add({
 	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
 })
 
+require "mini.pick".setup()
+require "oil".setup()
+
+vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
+vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
 vim.lsp.enable({ "lua_ls" })
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
 vim.cmd("colorscheme vague")
 vim.cmd(":hi statusline guibg=NONE")
