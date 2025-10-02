aa50878bab9b91b156ec79c587bdbda6b99fe596
artorias305 <105161738+artorias305@users.noreply.github.com>
2025-10-02 13:07:47 -0300

    Bare-bones setup

--- init.lua
+++ init.lua
@@ -11,6 +11,9 @@ vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
 
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
+
 vim.pack.add({
 	{ src = "https://github.com/vague2k/vague.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
@@ -19,12 +22,35 @@ vim.pack.add({
 	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
 })
 
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method('textDocument/completion') then
+			local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+			client.server_capabilities.completionProvider.triggerCharacters = chars
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end
+})
+vim.cmd("set completeopt+=noselect")
+
 require "mini.pick".setup()
 require "oil".setup()
 
 vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
 vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
-vim.lsp.enable({ "lua_ls" })
+vim.keymap.set('n', '<leader>e', ":Oil<CR>")
+
+vim.lsp.enable(
+	{
+		"lua_ls",
+		"clangd",
+		"tinymist",
+		"css-lsp",
+		"rust_analyzer"
+	}
+)
+
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
 vim.cmd("colorscheme vague")
