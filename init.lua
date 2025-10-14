f24e0ee56936f5836f5419eaaadf46143f5da535
artorias305 <105161738+artorias305@users.noreply.github.com>
2025-10-14 10:43:11 -0300

    pick buffers

--- init.lua
+++ init.lua
@@ -44,6 +44,7 @@ require "nvim-autopairs".setup()
 vim.keymap.set('n', '<leader>g', ':Pick grep_live<CR>')
 vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
 vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
+vim.keymap.set('n', '<leader>b', ':Pick buffers<CR>')
 vim.keymap.set('n', '<leader>e', ":Oil<CR>")
 
 vim.lsp.enable(
