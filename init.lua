3fc01ea94b24cf8c1e8447ef06e8659ed52db371
artorias305 <105161738+artorias305@users.noreply.github.com>
2025-12-30 14:49:57 -0300

    Initial commit

---
+++ init.lua
@@ -0,0 +1,91 @@
+-- Options
+vim.g.mapleader = " "
+
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.tabstop = 8
+vim.o.signcolumn = "yes"
+vim.o.winborder = "rounded"
+
+-- Define LSP servers to enable
+local servers = {"clangd", "lua_ls", "tinymist", "rust_analyzer", "pyright", "gopls"}
+
+-- Plugins setup
+vim.pack.add({
+	{ src = "https://github.com/folke/tokyonight.nvim" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/nvim-mini/mini.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	-- { src = "https://github.com/saghen/blink.cmp", version = 'v1.8.0' },
+	{ src = "https://github.com/akinsho/toggleterm.nvim" }
+})
+
+require("toggleterm").setup({
+	open_mapping = [[<c-\>]],
+	direction = "float",
+	float_opts = {
+		border = "curved"
+	}
+})
+
+require("oil").setup({
+	columns = {
+		"permissions",
+		"icon"
+	}
+})
+
+require("mini.pick").setup()
+require("mini.completion").setup()
+require("mini.pairs").setup()
+require("mini.surround").setup()
+
+-- Functions
+local function pack_clean()
+	local active_plugins = {}
+	local unused_plugins = {}
+
+	for _, plugin in ipairs(vim.pack.get()) do
+		active_plugins[plugin.spec.name] = plugin.active
+	end
+
+	for _, plugin in ipairs(vim.pack.get()) do
+		if not active_plugins[plugin.spec.name] then
+			table.insert(unused_plugins, plugin.spec.name)
+		end
+	end
+
+	if #unused_plugins == 0 then
+		print("No unused plugins.")
+		return
+	end
+
+	local choice = vim.fn.confirm("Remove unused plugins?", "&Yes\n&No", 2)
+	if choice == 1 then
+		vim.pack.del(unused_plugins)
+	end
+end
+
+
+-- LSP & Autocomplete
+for _, server in ipairs(servers) do
+	vim.lsp.enable(server)
+end
+
+-- NOTE: Testing mini completion
+-- require("blink.cmp").setup({
+-- 	fuzzy = { implementation = "prefer_rust" }
+-- })
+
+vim.cmd.colorscheme("tokyonight-night")
+
+-- Keymaps
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>pc", pack_clean)
+vim.keymap.set("n", "<leader>e", ":Oil<CR>")
+
+vim.keymap.set("n", "<leader>f", ":Pick files<CR>")
+vim.keymap.set("n", "<leader>h", ":Pick help<CR>")
+vim.keymap.set("n", "<leader>g", ":Pick grep_live<CR>")
+vim.keymap.set("n", "<leader>b", ":Pick buffers<CR>")
