b6c79f9ba0f85a572f0f83fff628e47f84167939
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-02-16 19:09:28 -0300

    new keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,3 +7,6 @@ map("n", "<leader>ff", builtin.find_files, { desc = 'Telescope find files' })
 map("n", "<leader>fb", builtin.buffers, { desc = 'Telescope buffers' })
 map("n", "<leader>fg", builtin.live_grep, { desc = 'Telescope live grep' })
 map("n", "<leader>fh", builtin.help_tags, { desc = 'Telescope help tags' })
+
+map("n", "<leader>tn", ":tabnew<CR>")
+map("n", "<leader>tx", ":tabclose<CR>")
