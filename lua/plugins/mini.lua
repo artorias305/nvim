c34c55c23d6a28d7ee4d511c10d14baf7ba0278d
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-02-16 11:50:12 -0300

    add mini.icons

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,8 @@
+return {
+        "nvim-mini/mini.nvim",
+        config = function()
+                require("mini.pairs").setup()
+                require("mini.surround").setup()
+                require("mini.icons").setup()
+        end
+}
