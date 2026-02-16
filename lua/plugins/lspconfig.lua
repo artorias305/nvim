ae3c25d862934074c50fc5d16ff4f7b261f02fed
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-02-16 12:03:52 -0300

    more lsps

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,11 @@
 return {
         "neovim/nvim-lspconfig",
         config = function()
-                vim.lsp.enable("lua_ls")
+                vim.lsp.enable({
+                        "lua_ls",
+                        "ts_ls",
+                        "clangd",
+                        "rust-analyzer"
+                })
         end
 }
