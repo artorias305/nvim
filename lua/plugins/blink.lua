a9496f1606a6c5eabe8adbda8e80042fb606242a
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-02-16 11:50:05 -0300

    add blink.cmp

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,16 @@
+return {
+        "saghen/blink.cmp",
+	version = "1.*",
+        opts = {
+                completion = {
+                        menu = {
+                                winblend = vim.o.pumblend
+                        }
+                },
+                signature = {
+                        window = {
+                                winblend = vim.o.pumblend
+                        }
+                }
+        }
+}
