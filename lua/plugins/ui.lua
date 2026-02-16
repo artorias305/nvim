8e91611187549466815330f36b23dcf983bb168b
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-02-16 11:57:24 -0300

    UI shit

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,32 @@
+return {
+        { "folke/noice.nvim", opts = {} },
+        { "rcarriga/nvim-notify" },
+        {
+                "folke/snacks.nvim",
+                opts = {
+                        scroll = { enabled = false }
+                },
+                keys = {}
+        },
+        {
+                "akinsho/bufferline.nvim",
+                event = "VeryLazy",
+                opts = {
+                        options = {
+                                mode = "tabs",
+                                show_buffer_close_icons = false,
+                                show_close_icon = false
+                        }
+                }
+        },
+        {
+                "nvim-lualine/lualine.nvim",
+                opts = {}
+        },
+        {
+                "folke/snacks.nvim",
+                opts = {
+                        dashboard = {}
+                }
+        }
+}
