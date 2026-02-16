f143747c14cf3a22e32e711ebf38c8c029bfc3c5
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-02-16 11:39:36 -0300

    add options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,33 @@
 vim.g.mapleader = " "
 
-vim.o.guicursor = ""
+vim.o.encoding = "utf-8"
+vim.o.fileencoding = "utf-8"
+
+vim.o.title = true
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.hlsearch = true
+vim.o.backup = false
+vim.o.showcmd=  true
+vim.o.cmdheight = 1
+vim.o.laststatus = 3
+vim.o.expandtab = true
+vim.o.scrolloff = 10
+vim.o.inccommand = "split"
+vim.o.ignorecase = true
+vim.o.smarttab = true
+vim.o.breakindent = true
+vim.o.shiftwidth = 8
+vim.o.tabstop = 8
+vim.o.wrap = false
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.splitkeep = "cursor"
+vim.o.mouse = ""
+
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+vim.g.lazyvim_picker = "telescope"
 
 vim.cmd("colorscheme solarized-osaka")
