57ded9b2fb91eb7be36673b6754b6eeaf922bd53
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-02-16 21:41:45 -0300

    add cyberdream colorscheme cause why not

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,27 @@
 return {
-	"craftzdog/solarized-osaka.nvim"
+        { "craftzdog/solarized-osaka.nvim" },
+        {
+                "scottmckendry/cyberdream.nvim",
+                lazy = false,
+                priority = 1000000,
+                opts = {
+                        transparent = true,
+                        borderless_pickers = false,
+                        saturation = 0.95,
+                        cache = true,
+                },
+                init = function()
+                        vim.cmd "colorscheme cyberdream"
+                        vim.api.nvim_set_hl(0, "TroubleNormal", { bg = "none", ctermbg = "none" })
+                        vim.api.nvim_set_hl(0, "TroubleNormalNC", { bg = "none", ctermbg = "none" })
+                        vim.api.nvim_set_hl(0, "TroubleNormal", { bg = "none", ctermbg = "none" })
+                        vim.api.nvim_set_hl(0, "TroubleNormalNC", { bg = "none", ctermbg = "none" })
+                        vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#3c4048", bg = "none" })
+                        vim.api.nvim_set_hl(0, "IndentBlanklineChar", { fg = "#7b8496" })
+                        vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#232429" })
+                        vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { bg = "#232429" })
+                        vim.api.nvim_set_hl(0, "TreesitterContextBottom", { bg = "#232429", underline = true })
+                        vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ffffff" })
+                end,
+        },
 }
