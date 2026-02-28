9227fc9bbf3886926f83149071697e88f4eb3db7
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-02-28 14:13:12 -0300

    Mini completion

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,26 +1,27 @@
 return {
-    "saghen/blink.cmp",
-    version = "1.*",
-    opts = {
-        fuzzy = {
-            implementation = "prefer_rust"
-        },
-        keymap = {
-            preset = "default",
-            ["<C-e>"] = { "hide" }
-        },
-        appearance = {
-            nerd_font_variant = "mono"
-        },
-        sources = {
-            default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-            providers = {
-                lazydev = {
-                    name = "LazyDev",
-                    module = "lazydev.integrations.blink",
-                    score_offset = 100
-                }
-            }
-        }
-    }
+	"saghen/blink.cmp",
+	version = "1.*",
+	enabled = false,
+	opts = {
+		fuzzy = {
+			implementation = "prefer_rust",
+		},
+		keymap = {
+			preset = "default",
+			["<C-e>"] = { "hide" },
+		},
+		appearance = {
+			nerd_font_variant = "mono",
+		},
+		sources = {
+			default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+			providers = {
+				lazydev = {
+					name = "LazyDev",
+					module = "lazydev.integrations.blink",
+					score_offset = 100,
+				},
+			},
+		},
+	},
 }
