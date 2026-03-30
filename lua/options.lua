0a17df0f94b9e17e4378eb67c8d011840062487b
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-03-30 09:43:37 -0300

    opt

--- lua/options.lua
+++ lua/options.lua
@@ -8,3 +8,19 @@ vim.opt.list = true
 vim.opt.listchars = vim.opt.listchars + "space:·"
 
 vim.o.mouse = "a"
+
+vim.o.winborder = "rounded"
+vim.o.hlsearch = true
+vim.o.backup = false
+vim.o.signcolumn = "yes"
+
+vim.o.termguicolors = true
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
+
+vim.o.cursorline = true
