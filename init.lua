a345dba9f5eaf0ecd6e350aabc19b0aee4dfebf7
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-04-19 09:56:00 -0300

    yo

--- init.lua
+++ init.lua
@@ -1,11 +1,38 @@
 vim.g.mapleader = " "
 
 local map = vim.keymap.set
+local theme = "tokyonight-night"
 
 -- Options
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.signcolumn = "yes"
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+vim.o.showmode = false
+vim.o.mouse = 'a'
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.inccommand = 'split'
+vim.o.cursorline = true
+vim.o.scrolloff = 10
+vim.o.confirm = true
+vim.diagnostic.config {
+	update_in_insert = false,
+	severity_sort = true,
+	float = { border = 'rounded', source = 'if_many' },
+	underline = { severity = { min = vim.diagnostic.severity.WARN } },
+	virtual_text = true,
+	virtual_lines = false,
+	jump = { float = true }
+}
 
 -- Plugins
 vim.pack.add({
@@ -16,23 +43,41 @@ vim.pack.add({
 	"https://github.com/mason-org/mason-lspconfig.nvim",
 	"https://github.com/nvim-telescope/telescope.nvim",
 	"https://github.com/nvim-telescope/telescope-fzf-native.nvim",
-	"https://github.com/nvim-lua/plenary.nvim"
+	"https://github.com/nvim-lua/plenary.nvim",
+	"https://github.com/vague2k/vague.nvim",
+	"https://github.com/saghen/blink.cmp",
+	"https://github.com/nvim-tree/nvim-web-devicons",
+	"https://github.com/folke/tokyonight.nvim"
 })
 
 -- Setups
 require("oil").setup()
 require("mini.pairs").setup()
+require("mini.statusline").setup()
 require("mason").setup()
+servers = { "rust_analyzer", "gopls", "clangd", "lua_ls", "basedpyright" }
 require("mason-lspconfig").setup({
-	ensure_installed = {
-		"rust_analyzer",
-		"gopls",
-		"clangd",
-		"lua_ls",
-		"ts_ls",
-		"basedpyright"
-	}
+	ensure_installed = servers
 })
+
+require("blink.cmp").setup({
+	keymap = {
+		preset = 'default',
+	},
+	appearance = {
+		nerd_font_variant = 'mono',
+	},
+	completion = {
+		documentation = { auto_show = false, auto_show_delay_ms = 500 },
+	},
+	sources = {
+		default = { 'lsp', 'path', 'snippets' },
+	},
+	fuzzy = { implementation = 'lua' },
+	signature = { enabled = true },
+})
+
+vim.lsp.enable(servers)
 
 -- Keymaps
 local builtin = require("telescope.builtin")
@@ -43,3 +88,16 @@ map("n", "<leader>ff", builtin.find_files)
 map("n", "<leader>fg", builtin.live_grep)
 map("n", "<leader>fh", builtin.help_tags)
 map("n", "<leader>fc", builtin.git_commits)
+map("n", "<leader>ft", builtin.colorscheme)
+map("n", "<leader>m", builtin.git_branches)
+map("n", "<leader>lf", vim.lsp.buf.format)
+map("n", "<C-c>", ":noh<CR>")
+map("n", "<leader>q", vim.diagnostic.setloclist)
+
+-- Cmds
+vim.cmd.colorscheme(theme)
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = 'Highlight when yanking (copying) text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function() vim.hl.on_yank() end,
+})
