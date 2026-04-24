38b5e153ddfddc19fd065cc1046a4ebd64f24d7c
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-04-24 19:26:22 -0300

    a

--- init.lua
+++ init.lua
@@ -6,11 +6,22 @@ vim.g.mapleader = " "
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.signcolumn = "yes"
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
 vim.o.winborder = "rounded"
-vim.o.showtabline = 2
+vim.o.breakindent = true
+vim.o.showmode = false
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.splitright = true
+vim.o.splitbelow = true
 vim.o.confirm = true
+vim.o.confirm = true
+vim.o.termguicolors = true
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = 'Highlight when yanking (copying) text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function() vim.hl.on_yank() end,
+})
 
 vim.pack.add({
 	"https://github.com/vague-theme/vague.nvim",
@@ -24,28 +35,8 @@ vim.pack.add({
 	"https://github.com/nvim-tree/nvim-web-devicons",
 	"https://github.com/nvim-telescope/telescope.nvim",
 	"https://github.com/folke/tokyonight.nvim",
-	-- "https://github.com/saghen/blink.lib",
-	-- "https://github.com/saghen/blink.cmp"
+	"https://github.com/chomosuke/typst-preview.nvim",
 })
-
--- local cmp = require("blink.cmp")
--- cmp.build():wait(60000)
--- cmp.setup({
--- 	keymap = {
--- 		preset = 'default'
--- 	},
--- 	appearance = {
--- 		nerd_font_variant = 'mono'
--- 	},
--- 	completion = {
--- 		documentation = { auto_show = false, auto_show_delay_ms = 500 }
--- 	},
--- 	sources = {
--- 		default = { 'lsp', 'path', 'snippets' }
--- 	},
--- 	fuzzy = { implementation = 'lua' },
--- 	signature = { enabled = true }
--- })
 
 vim.api.nvim_create_autocmd('LspAttach', {
 	group = vim.api.nvim_create_augroup('my.lsp', {}),
@@ -64,12 +55,12 @@ vim.cmd [[set completeopt+=menuone,noselect,popup]]
 
 local builtin = require("telescope.builtin")
 
-local themes = { 'markdown', 'lua', 'rust', 'typst', 'typescript', 'javascript', 'c', 'cpp', 'python', 'go' }
+local langs = { 'markdown', 'lua', 'rust', 'typst', 'typescript', 'javascript', 'c', 'cpp', 'python', 'go' }
 
 require("nvim-treesitter").install(themes)
 
 vim.api.nvim_create_autocmd('FileType', {
-	pattern = themes,
+	pattern = langs,
 	callback = function() vim.treesitter.start() end
 })
 
@@ -105,7 +96,22 @@ require("mason-lspconfig").setup({
 })
 vim.lsp.enable(servers)
 
-vim.cmd.colorscheme('vague')
+local theme_state_file = vim.fs.joinpath(vim.fn.stdpath("state"), "last_theme.txt")
+
+local function load_last_theme()
+	local file = io.open(theme_state_file, "r")
+	if file then
+		local theme = file:read("*l")
+		file:close()
+		if theme and theme ~= "" and pcall(vim.cmd.colorscheme, theme) then
+			return
+		end
+	end
+
+	vim.cmd.colorscheme("tokyonight-night")
+end
+
+load_last_theme()
 
 require("mini.pairs").setup()
 require("mini.surround").setup()
@@ -127,6 +133,7 @@ map("n", "<leader>sq", builtin.diagnostics)
 map("n", "<C-c>", ":noh<CR>")
 map("n", "<leader>q", vim.diagnostic.setloclist)
 map("n", "<leader>v", ":e $MYVIMRC<CR>")
+map("n", "<leader>tp", ":TypstPreviewToggle<CR>")
 
 vim.diagnostic.config {
 	update_in_insert = false,
@@ -140,16 +147,42 @@ vim.diagnostic.config {
 	jump = { float = true }
 }
 
+local function apply_custom_theme_overrides()
+	if vim.g.colors_name == 'tokyonight-night' then
+		vim.o.list = true
+		vim.o.cursorline = true
+		vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+	elseif vim.g.colors_name == 'vague' then
+		vim.o.list = false
+		vim.o.cursorline = false
+		vim.o.showtabline = 2
+		vim.api.nvim_set_hl(0, "TabLineFill", { bg = "#2b2a33" })
+		vim.api.nvim_set_hl(0, "TabLineSel", { bg = "#d2d2d2", fg = "#2b2a33" })
+		vim.api.nvim_set_hl(0, "TabLine", { fg = "#d2d2d2", bg = "#2b2a33" })
+		vim.api.nvim_set_hl(0, "StatusLine", { bg = "#6e6a86", fg = "#d0cdda" })
+	else
+		vim.o.list = false
+		vim.o.cursorline = false
+	end
+end
+
 vim.api.nvim_create_autocmd('ColorScheme', {
 	pattern = "*",
 	callback = function()
-		if vim.g.colors_name == 'tokyonight-night' then
-			vim.o.list = true
-			vim.o.cursorline = true
-			vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-		else
-			vim.o.list = false
-			vim.o.cursorline = false
+		local state_dir = vim.fn.stdpath("state")
+		vim.fn.mkdir(state_dir, "p")
+
+		local file = io.open(theme_state_file, "w")
+		if file then
+			file:write(vim.g.colors_name)
+			file:close()
 		end
 	end
 })
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+	pattern = "*",
+	callback = apply_custom_theme_overrides
+})
+
+apply_custom_theme_overrides()
